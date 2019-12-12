e0328b8277df00a8f01fe0034217605f09d9c52e
PabWork <p.clevelandortega@gmail.com>
2019-12-12 16:47:05 -0300

    Ejercicios

--- Tarea_3/arboles.py
+++ Tarea_3/arboles.py
@@ -1,11 +1,14 @@
+# coding=utf-8
 # para crear copias de los arboles
 from copy import deepcopy
 # random
 import random
 
+
 # esta funcion dice si el argumento es una funcion o no
 def is_function(f):
     return hasattr(f, "__call__")
+
 
 # esto nos permite recorrer una lista de a pedazos
 # ejemplo el input es [1,2,3,4,5,6,7,8]
@@ -15,6 +18,7 @@ def chunks(iterable, n):
     for i in range(0, len(iterable), n):
         yield iterable[i:i + n]
 
+
 class Node:
     def __init__(self, function):
         # nos aseguramos que el nodo recibe una funcion
@@ -23,9 +27,10 @@ class Node:
         # esto nos permite contar cuantos argumentos recibe la funcion
         self.num_arguments = function.__code__.co_argcount
         self.arguments = []
-        
+        self.feasible = True
+
     # funcion para evaluar un nodo (calcular el resultado)
-    def eval(self):
+    def eval(self, vals=None):
         # es importante chequear que los argumentos que nos dieron
         # coincidan con los argumentos que necesitamos
         assert len(self.arguments) == self.num_arguments
@@ -38,19 +43,32 @@ class Node:
         # esto se llama `unpacking`.
         # lo necesitamos porque nuestra funcion recibe N argumentos
         # no una lista de tamaño N.
-        return self.operation(*[node.eval() for node in self.arguments])
-    
+        if vals is None:
+            return self.operation(*[node.eval() for node in self.arguments])
+        elif type(vals) == dict:
+            return self.operation(*[node.eval(vals) for node in self.arguments])
+        else:
+            raise ValueError("vals debe ser un diccionario")
+
+    # funcion para evaluar si un nodo es feasible
+    def isfeasible(self):
+        # es importante chequear que los argumentos que nos dieron
+        # coincidan con los argumentos que necesitamos
+        assert len(self.arguments) == self.num_arguments
+        # evaluamos los argumentos, y luego los pasamos como argumentos a        
+        return bool(self.feasible * np.product([node.isfeasible() for node in self.arguments]))
+
     # hace una lista con todos los hijos
     def serialize(self):
         l = [self]
         for node in self.arguments:
             l.extend(node.serialize())
         return l
-    
+
     # copia al nodo
     def copy(self):
         return deepcopy(self)
-    
+
     # reemplaza el nodo por otro
     def replace(self, otherNode):
         # aqui estamos haciendo algo medio hacky.
@@ -66,6 +84,7 @@ class Node:
 # esta clase representa todos los nodos quetienen 2 argumentos
 class BinaryNode(Node):
     num_args = 2
+
     def __init__(self, function, left, right):
         # revisamos que todo sea un nodo y agregamos a las lista de
         # argumentos
@@ -74,64 +93,101 @@ class BinaryNode(Node):
         super(BinaryNode, self).__init__(function)
         self.arguments.append(left)
         self.arguments.append(right)
-        
+        self.feasible = True
+
 
 class AddNode(BinaryNode):
     def __init__(self, left, right):
-        def _add(x,y):
+        def _add(x, y):
             return x + y
         super(AddNode, self).__init__(_add, left, right)
-        
+        self.feasible = True
+
     # esta es la funcion que define como se mostrara el nodo
     # como es un nodo que REPResenta la suma, lo mostramos como suma
     def __repr__(self):
         return "({} + {})".format(*self.arguments)
-        
-    
+
+
 class SubNode(BinaryNode):
     def __init__(self, left, right):
-        def _sub(x,y):
+        def _sub(x, y):
             return x - y
+
         super(SubNode, self).__init__(_sub, left, right)
-        
+        self.feasible = True
+
     def __repr__(self):
         return "({} - {})".format(*self.arguments)
-    
-    
+
+
 class MaxNode(BinaryNode):
     def __init__(self, left, right):
-        def _max(x,y):
-            return max(x,y)
+        def _max(x, y):
+            return max(x, y)
+
         super(MaxNode, self).__init__(_max, left, right)
-        
+        self.feasible = True
+
     def __repr__(self):
         return "max({{{}, {}}})".format(*self.arguments)
 
 
 class MultNode(BinaryNode):
     def __init__(self, left, right):
-        def _mult(x,y):
+        def _mult(x, y):
             return x * y
+
+
+
         super(MultNode, self).__init__(_mult, left, right)
-        
+        self.feasible = True
+
     def __repr__(self):
         return "({} * {})".format(*self.arguments)
-    
-    
+
+
+class DivNode(BinaryNode):
+    def __init__(self, left, right):
+        def _div(x, y):
+            try:
+                return x / y
+            except:
+                self.feasible = False
+                return 0
+
+        super(DivNode, self).__init__(_div, left, right)
+        self.feasible = True
+
+    def __repr__(self):
+        return "({} / {})".format(*self.arguments)
+
+
 class TerminalNode(Node):
     # Este nodo representa una hoja de arbol. Es el nodo terminal
     # por lo que no tiene argumentos
     num_args = 0
+
     def __init__(self, value):
         # igual tenemos que representarlo como una funcion, por como
         # diseñamos el programa. Pero aqui va a ser una funcion vacia
         def _nothind(): pass
+
         super(TerminalNode, self).__init__(_nothind)
         self.value = value
-        
+        self.feasible = True
+
+
     def __repr__(self):
         return str(self.value)
-    
-    def eval(self):
-        # la evaluacion de un nodo terminal es el valor que contiene
-        return self.value
+
+    def eval(self, vals=None):
+        if vals is None:
+            return self.value
+        elif type(vals) == dict:
+            return vals[self.value]
+        else:
+            raise ValueError("vals debe ser un diccionario")
+
+    def isfeasible(self):
+        return self.feasible
